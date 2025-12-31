e40f130f7b13893f28a4442beed9de91dc316410
MikeMull10 <50462800+MikeMull10@users.noreply.github.com>
2025-12-30 23:22:09 -0500

    Update plotting.py

--- sections/plotting.py
+++ sections/plotting.py
@@ -1,6 +1,7 @@
 from PySide6.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout
 
 from components.formsections import VariablesSection, VariableItem, FunctionsSection, FunctionItem
+from testing.fnc_objects import Variable, Function
 
 from qfluentwidgets import SmoothScrollArea, FluentIconBase, Theme, theme
 
@@ -32,3 +33,39 @@ class PlottingPage(QWidget):
         self.formulation_layout.addStretch()
 
         scroll.setLayout(self.formulation_layout)
+    
+    def populate_graph(self):
+        ...
+    
+    def get_surface_plot(self, variables: list[Variable], function: Function):
+        if len(self.variables) != 2:
+            return None
+
+        v = self.variables
+        x1 = np.arange(
+            v["X1"][0], v["X1"][1] + v["X1"][0] / 10, v["X1"][0] / 10
+        )
+        x2 = np.arange(
+            v["X2"][0], v["X2"][1] + v["X2"][0] / 10, v["X2"][0] / 10
+        )
+
+        x = np.meshgrid(x1, x2)
+
+        func_str = clean(self.functions[function_to_graph], 2)
+
+        func = eval(func_str)
+
+        # Create a 3D plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection="3d")
+
+        # Plot the surface
+        ax.plot_surface(*x, func, cmap="viridis")
+
+        # Set labels
+        ax.set_xlabel("X1")
+        ax.set_ylabel("X2")
+        ax.set_zlabel(function_to_graph)
+
+        # return the plot
+        return fig
