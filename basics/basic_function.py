bd30c42fee30df5d18dfd634ab0061d30fc83678
MikeMull10 <50462800+MikeMull10@users.noreply.github.com>
2026-01-05 16:47:07 -0500

    Missed a Few

--- basics/basic_function.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-def resource_path(relative_path):
-    """ Get the absolute path to the resource, works for dev and for PyInstaller bundled executables. """
-    if hasattr(sys, '_MEIPASS'):
-        # PyInstaller stores files in a temporary folder (_MEIPASS) during execution
-        return os.path.join(sys._MEIPASS, relative_path)
-    return os.path.join(os.path.abspath("."), relative_path)
